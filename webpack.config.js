b8d6ffcdc1e225bae6f548d5a5ec0229faf5c65d
Herve07h22 <contact@storeed.me>
2018-09-25 11:30:31 +0200

    with purifyCSS

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,13 @@ const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
 
 module.exports = {
   entry: './src/styles.css',
@@ -29,6 +36,17 @@ module.exports = {
       filename: 'index.html',
       template: 'src/index.html',
     }),
-    new CopyWebpackPlugin([ { from: './assets/*' } ])
+    new CopyWebpackPlugin([ { from: './assets/*' } ]),
+    new PurgecssPlugin({
+      paths: ['./src/index.html'],
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          // Specify the file extensions to include when scanning for
+          // class names.
+          extensions: ["html", "js", "php", "vue"]
+        }
+      ]
+    })
   ]
 }
